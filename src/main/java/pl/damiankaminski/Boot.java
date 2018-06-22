ffc99dca3d77bad9df10202bd773104ea18cfd02
Damian <Mizuirokoala@gmail.com>
2018-06-22 16:56:54 +0200

    update Boot, create text-sqlcode, add User.class

--- src/main/java/pl/damiankaminski/Boot.java
+++ src/main/java/pl/damiankaminski/Boot.java
@@ -3,6 +3,7 @@ package pl.damiankaminski;
 import pl.damiankaminski.model.User;
 
 import java.sql.*;
+import java.util.Random;
 
 public class Boot {
     public static void main(String[] args) {
@@ -10,10 +11,15 @@ public class Boot {
         String username = "root";
         String password = "coderslab";
         try (Connection connection = DriverManager.getConnection(jdbc, username,password)) {
-            User user =User.findById(connection,1);
-            System.out.println(user);
+            User jan =User.findById(connection,1);
+    //        System.out.println(user);
+            jan.setPassword("admin"+new Random().nextInt());
+            jan.save(connection);
+            System.out.println(jan);
 
-
+            User basia = new User("barbaaa", "barna@gma"+ new Random().nextInt()+"gmail", "test2");
+            basia.save(connection);
+            System.out.println(basia);
 
             //            String query = "SELECT 'Hello, Database'";
 //            PreparedStatement sql = connection.prepareStatement(query);
