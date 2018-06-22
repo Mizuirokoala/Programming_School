b86278182741158033f339e766956fd24fc231cd
Damian <Mizuirokoala@gmail.com>
2018-06-22 16:14:27 +0200

    update Boot, create sql

--- src/main/java/pl/damiankaminski/Boot.java
+++ src/main/java/pl/damiankaminski/Boot.java
@@ -1,7 +1,31 @@
 package pl.damiankaminski;
 
+import pl.damiankaminski.model.User;
+
+import java.sql.*;
+
 public class Boot {
     public static void main(String[] args) {
-        System.out.println("hello world");
+        String jdbc = "jdbc:mysql://localhost:3306/programming_school?useSSL=false";
+        String username = "root";
+        String password = "coderslab";
+        try (Connection connection = DriverManager.getConnection(jdbc, username,password)) {
+            User user =User.findById(connection,1);
+            System.out.println(user);
+
+
+
+            //            String query = "SELECT 'Hello, Database'";
+//            PreparedStatement sql = connection.prepareStatement(query);
+//            ResultSet rs = sql.executeQuery();
+//            while (rs.next()) {
+//                String hello = rs.getString(1);
+//                System.out.println(hello);
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
     }
+
 }
